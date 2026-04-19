3201e0612982fbd38e47fd3be50834f88f06bb07
DANIA-DESKTOP\user <ainuldania@gmail.com>
2026-04-19 23:15:09 +0800

    Project created

---
+++ DrinksInfo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DrinksInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
