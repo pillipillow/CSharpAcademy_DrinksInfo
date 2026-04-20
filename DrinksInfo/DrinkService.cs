1585cb83d004dbeab1a8ac6cb690d294f2e14ac3
DANIA-DESKTOP\user <ainuldania@gmail.com>
2026-04-20 20:28:21 +0800

    Added GetDrinksByCategory method

--- DrinksInfo/DrinkService.cs
+++ DrinksInfo/DrinkService.cs
@@ -1,5 +1,6 @@
 ﻿using DrinksInfo.Models;
 using Newtonsoft.Json;
+using System.Web;
 
 namespace DrinksInfo
 {
@@ -10,7 +11,7 @@ namespace DrinksInfo
             BaseAddress = new Uri("http://www.thecocktaildb.com/api/json/v1/1/")
         };
 
-        internal async Task GetCategories()
+        internal async Task<List<Category>> GetCategories()
         {
             var response = await client.GetAsync("list.php?c=list");
 
@@ -21,10 +22,22 @@ namespace DrinksInfo
             var serialize = JsonConvert.DeserializeObject<Categories>(rawResponse);
             List<Category> categories = serialize.categoriesList;
 
-            foreach (var c in categories)
-            { 
-                Console.WriteLine($"- {c.strCategory}");
-            }
+            return categories; 
+        }
+
+        internal async Task<List<Drink>> GetDrinksByCategory(string category)
+        { 
+            var response = await client.GetAsync($"filter.php?c={HttpUtility.UrlEncode(category)}");
+
+            response.EnsureSuccessStatusCode();
+
+            var rawResponse = await response.Content.ReadAsStringAsync();
+
+            var serialize = JsonConvert.DeserializeObject<Drinks>(rawResponse);
+            List<Drink> drinks = serialize.drinksList;
+
+            return drinks;
+
         }
     }
 }
---
+++ DrinksInfo/Models/Drink.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace DrinksInfo.Models
+{
+    public class Drink
+    {
+        public string idDrink { get; set; }
+        public string strDrink { get; set; }
+    }
+
+    public class Drinks
+    {
+        [JsonProperty("drinks")]
+        public List<Drink> drinksList { get; set; }
+    }
+}
